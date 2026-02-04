afe7981aef59ee80516e70a36aab14c06c623647
NovAldesign <williams.lavon@gmail.com>
2026-02-04 16:25:44 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+// Imports
+import express from "express";
+import { loqReq, globalErr } from 
+
+// Set Ups
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || "";
+
+
+// (Request) Middleswares
+
+
+// Routes
+
+
+// Global Error Handling Middleware
+
+
+// Listener
