0ef4ce5d01b215a7a6171bca1f6779b6e56e241e
NovAldesign <williams.lavon@gmail.com>
2026-02-04 19:20:47 -0500

    completed schema

--- models/applicantSchema.js
+++ models/applicantSchema.js
@@ -1,5 +1,49 @@
 import mongoose from "mongoose";
 
-const applicantSchema = new mongoose.Schema({
-    
-})
+const applicantSchema = new mongoose.Schema(
+    {
+        category: "applicants",
+
+        name:
+        {
+            type: String,
+            required: true,
+        },
+        email:
+        {
+            type: String,
+            unique: true,
+            required: true,
+        },
+
+        phone:
+        {
+            type: String,
+            unique: true,
+            required: true,
+        },
+
+        dob:
+        {
+            type: String,
+            required: true,
+        },
+        industry:
+        {
+            type: String,
+            required: true,
+
+        },
+        tier:
+            { type: String },
+
+        status:
+            { type: String },
+
+        isFirstTimeFounder:
+            { type: Boolean },
+
+
+    });
+
+export default mongoose.model("Applicants", applicantSchema);
